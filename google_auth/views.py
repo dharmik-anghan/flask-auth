99c22e99ddcab9cdb59bfc5371b458e5d375afd7
Dharmik Anghan <dharmikanghan09@gmail.com>
2023-12-12 14:28:50 +0530

    [Dharmik] Save use to database

--- google_auth/views.py
+++ google_auth/views.py
@@ -2,6 +2,7 @@ from flask import url_for, session, redirect, Blueprint, jsonify
 from extentions import oauth, db
 from models.user import User
 from api.schemas.user import UserCreateSchema, UserSchema
+from random import randint, choice
 
 google_oauth = Blueprint("google_oauth", __name__, url_prefix="/auth/google")
 
@@ -25,7 +26,40 @@ def auth():
 def register():
     user = session.get("user")
 
-    return {"msg": "User Created", "user": user}
+    db_user = User.query.filter_by(email=user["email"]).first()
+
+    if db_user:
+        return jsonify({"msg": "Email already registered"})
+
+    temp_user = {}
+    user = jsonify(user)
+    user = user.get_json()
+    username = user.get("email").split("@")
+    password = (
+        (username[0][:3])
+        + (f"{choice(seq=['@', '#', '^', '&', '$'])}")
+        + (f"{randint(10000,100000)}")
+        + f"{choice(list('QWERTYUIOPLKJHGFDASZXCVBNM'))}"
+    )
+
+    temp_user["email"] = user.get("email")
+    temp_user["full_name"] = user.get("name")
+    temp_user["password"] = password
+    temp_user["username"] = username[0]
+    temp_user["is_verified"] = True if user.get("email_verified") == True else False
+
+    schema = UserCreateSchema()
+    db_user = schema.load(temp_user)
+    db.session.add(db_user)
+    db.session.commit()
+
+    schema = UserSchema()
+
+    return {
+        "msg": "User Created",
+        "user": temp_user,
+        "security_alert": f"{password}, This is your system generated password, change it as soon as possible",
+    }
 
 
 @google_oauth.route("/logout")
