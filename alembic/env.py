7d68878153de8365b9bdb240583a3c747d10ace1
Dharmik Anghan <dharmikanghan09@gmail.com>
2023-12-18 12:20:49 +0530

    [Dharmik] alembic table created

--- alembic/env.py
+++ alembic/env.py
@@ -4,7 +4,8 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from extentions import db
+from app import db
+from models.user import User    
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
