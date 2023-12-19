39e5a3c4050b6821f0a6242cc14362cb3adea8ae
Dharmik Anghan <dharmikanghan09@gmail.com>
2023-12-19 16:56:08 +0530

    [Dharmik], Refresh, revoke set

--- auth/views.py
+++ auth/views.py
@@ -59,7 +59,7 @@ def login():
 @jwt_required(refresh=True)
 def refresh():
     user_id = get_jwt_identity()
-    access_token = create_access_token(identity=user_id,fresh=False)
+    access_token = create_access_token(identity=user_id)
     add_token_to_database(access_token)
     return jsonify({"access_token": access_token}), 200
 
@@ -67,7 +67,6 @@ def refresh():
 @auth_blueprint.route("/revoke_access", methods=["DELETE"])
 @jwt_required()
 def revoke_access_token():
-    print(get_jwt())
     jti = get_jwt()["jti"]
     user_id = get_jwt_identity()
     revoke_token(jti, user_id)
