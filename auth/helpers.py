e6e0c5f3910e75cbe45571aaee63cda3fcb7bd29
Dharmik Anghan <dharmikanghan09@gmail.com>
2023-12-19 16:01:17 +0530

    [Dharmik] functions added for refresh and revoke

--- auth/helpers.py
+++ auth/helpers.py
@@ -2,7 +2,8 @@ from extentions import db
 from models.auth import TokenBlockList
 from flask import current_app as app
 from flask_jwt_extended import decode_token
-from datetime import datetime
+from datetime import datetime, timezone
+from sqlalchemy.exc import NoResultFound
 
 def add_token_to_database(encoded_token):
     decoded_token = decode_token(encoded_token=encoded_token)
@@ -20,3 +21,20 @@ def add_token_to_database(encoded_token):
 
     db.session.add(db_token)
     db.session.commit()
+
+def revoke_token(token_jti, user_id):
+    try:
+        token = TokenBlockList.query.filter_by(jti=token_jti, user_id=user_id).one()
+        token.revoked_at = datetime.now(timezone.utc)
+        db.session.commit()
+    except NoResultFound:
+        raise Exception(f"Could not find token {token_jti}")
+    
+def is_token_revoked(jwt_payload):
+    jti = jwt_payload["jti"]
+    user_id = jwt_payload[app.config.get("JWT_IDENTITY_CLAIM")]
+    try:
+        token = TokenBlockList.query.filter_by(jti=jti, user_id=user_id).one()
+        return token.revoked_at is not None
+    except NoResultFound:
+        raise Exception(f"Could not find token {jti}")
