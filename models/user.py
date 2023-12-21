8d160320d86944b97dba87f4280fbd20a3dcfd81
Dharmik Anghan <dharmikanghan09@gmail.com>
2023-12-21 10:44:50 +0530

    [Dharmik] id to UUID changed

--- models/user.py
+++ models/user.py
@@ -2,19 +2,22 @@ from extentions import db, pwd_context
 
 from datetime import datetime
 from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.dialects.postgresql import UUID
 from models.otp import OTPDatabase
 from models.auth import TokenBlockList
+import uuid
 
 
 class User(db.Model):
     __tablename__ = "users"
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     email = db.Column(db.String(100), unique=True, nullable=False)
     full_name = db.Column(db.String(40), nullable=False)
     username = db.Column(db.String(15), unique=True, nullable=False)
     _password = db.Column("password", db.String(255), nullable=False)
 
+    is_public = db.Column(db.Boolean, nullable=False)
     is_verified = db.Column(db.Boolean, default=False, nullable=False)
     is_active = db.Column(db.Boolean, default=True, nullable=False)
     is_deleted = db.Column(db.Boolean, default=False, nullable=False)
