08a993d83560d69cf70d9545ee670c3fb099ee98
Dharmik Anghan <dharmikanghan09@gmail.com>
2023-12-22 09:38:11 +0530

    [Dharmik] model post_comment_likes added

---
+++ models/post_comment_likes.py
@@ -0,0 +1,27 @@
+from extentions import db
+from datetime import datetime
+
+
+class PostCommentLikes(db.Model):
+    __tablename__ = "comment_likes"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    comment_id = db.Column(
+        db.Integer, db.ForeignKey("post_comments.id"), nullable=False
+    )
+    post_id = db.Column(db.Integer, db.ForeignKey("posts.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    created_at = db.Column(
+        db.DateTime(timezone=True), nullable=False, default=datetime.now
+    )
+
+    # post.py
+    post_table = db.relationship("Post", back_populates="post_comment_likes_table")
+
+    # user.py
+    user_table = db.relationship("User", back_populates="post_comment_likes_table")
+
+    # comment.py
+    post_comment_table = db.relationship(
+        "PostComment", back_populates="post_comment_likes_table"
+    )
