59d9342cab8d73507adf67aa72eb8833571ee4a8
Dharmik Anghan <dharmikanghan09@gmail.com>
2023-12-27 14:58:33 +0530

    [Dharmik] reply to comment feature added

---
+++ api/schemas/post_comment_reply.py
@@ -0,0 +1,27 @@
+from extentions import ma
+from models.post_comment_reply import PostCommentReply
+
+
+class PostCommentSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = PostCommentReply
+        load_instance = True
+
+
+class PostCreateCommentSchema(PostCommentSchema):
+    class Meta(PostCommentSchema.Meta):
+        fields = ["post_id", "comment_reply", "user_id", "comment_id"]
+
+
+class PostCommentReplyResponseSchema(PostCommentSchema):
+    class Meta(PostCommentSchema.Meta):
+        fields = [
+            "id",
+            "post_id",
+            "comment_id",
+            "comment_reply",
+            "user_id",
+            "created_at",
+            "modified_at",
+            "reply_like_count"
+        ]
