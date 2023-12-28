1a54dfe1975706bd357ea0fe330e6a160a2d8b1c
Dharmik Anghan <dharmikanghan09@gmail.com>
2023-12-28 16:12:47 +0530

    Name changed

--- models/follower_following.py
+++ models/follower_following.py
@@ -5,10 +5,10 @@ class FollowerFollowing(db.Model):
     __tablename__ = "followers_followings"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False, index=True)
     followed_by = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False, index=True)
+    followed_to = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False, index=True)
     followed_at = db.Column(db.DateTime(timezone=True), nullable=False, default=datetime.now)
 
-    user_following_table = db.relationship("User", back_populates="following", foreign_keys=[user_id])
+    user_following_table = db.relationship("User", back_populates="following", foreign_keys=[followed_to])
     user_follower_table = db.relationship("User", back_populates="follower", foreign_keys=[followed_by])
     
