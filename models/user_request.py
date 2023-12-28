3d1cb3df343db74c7fd45160398d0fb831946015
Dharmik Anghan <dharmikanghan09@gmail.com>
2023-12-28 16:15:10 +0530

    removed unnecessary column

--- models/user_request.py
+++ models/user_request.py
@@ -5,10 +5,8 @@ class RequestToUser(db.Model):
     __tablename__ = "request_to_user"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), index=True, nullable=False)
+    requested_to = db.Column(db.Integer, db.ForeignKey("users.id"), index=True, nullable=False)
     requested_by = db.Column(db.Integer, db.ForeignKey("users.id"), index=True, nullable=False)
-    request_accepted = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime(timezone=True), default=datetime.now)
-
-    user_request_to_table = db.relationship("User", back_populates="user_request_to", foreign_keys=[user_id])
+    user_request_to_table = db.relationship("User", back_populates="user_request_to", foreign_keys=[requested_to])
     user_request_by_table = db.relationship("User", back_populates="user_request_by", foreign_keys=[requested_by])
